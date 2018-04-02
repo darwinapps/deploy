fa770207f521301603f697d0a28a13ed0803ea2d
Aleksandr Guidrevitch <aguidrevitch@darwinapps.com>
2018-04-02 17:57:11 +0300

    better git handling

--- deploy.sh
+++ deploy.sh
@@ -116,7 +116,7 @@ function upload_dump() {
 function gitcmd() {
     if [[ $REPOSITORY_KEY != "" ]]; then
         GIT=$(get_git_cli "$REPOSITORY_KEY")
-        docker run -ti --rm -v $PWD:/git -e GIT_SSH_COMMAND='ssh  -o "StrictHostKeyChecking no" -i /id_rsa' $GIT "$@"
+        docker run -ti --rm -v $PWD:/git -e GIT_SSH_COMMAND='ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" -i /id_rsa' $GIT "$@"
     else
         git "$@"
     fi
