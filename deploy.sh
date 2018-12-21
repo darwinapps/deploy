72a59d7dd80ee3aa5c395bd950d1c2c3f3402cae
Aleksandr Guidrevitch <aguidrevitch@darwinapps.com>
2018-12-21 20:47:43 +0300

    minor fix

--- deploy.sh
+++ deploy.sh
@@ -119,7 +119,7 @@ function get_latest_db_dump_pantheon {
 
 function get_latest_files_from_aws() {
     FILENAME=${1:-files.tgz}
-    if [[ ! -f remote-files/files.tgz ]]; then
+    if [[ ! -f remote-files/latest.tgz ]]; then
         if [[ ! -d remote-files/ ]]; then
             mkdir remote-files/
         fi
