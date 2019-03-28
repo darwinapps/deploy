0f490c32b5e92f0a81d7b828f169a12e100cf255
Aleksandr Guidrevitch <aguidrevitch@darwinapps.com>
2019-03-28 17:42:33 +0300

    getting rid of compromised credentials

---
+++ scripts/yii2-dotenv/docker-entrypoint.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -euo pipefail
+
+if [[ "$1" == apache2* ]] || [ "$1" == php-fpm ]; then
+	if [ "$(id -u)" = '0' ]; then
+		case "$1" in
+			apache2*)
+				user="${APACHE_RUN_USER:-www-data}"
+				group="${APACHE_RUN_GROUP:-www-data}"
+				;;
+			*) # php-fpm
+				user='www-data'
+				group='www-data'
+				;;
+		esac
+	else
+		user="$(id -u)"
+		group="$(id -g)"
+	fi
+
+	if [ ! -e .env ]; then
+		cat <<EOF > .env
+DB_NAME=${MYSQL_DATABASE}
+DB_PASS=${MYSQL_PASSWORD}
+DB_USER=${MYSQL_USER}
+DB_HOST=${MYSQL_HOST}
+PROTOCOL=http
+ENVIRONMENT=development
+#SMTP_HOST=smtp.sendgrid.net
+#SMTP_USERNAME=appmailer
+#SMTP_PASSWORD=
+#SMTP_PORT=587
+#SMTP_ENCRYPTION=tls
+#MAIL_SENDER_ADDRESS=thehub@youthmarketing.com
+#MAIL_SENDER_NAME='The Hub'
+#MAIL_ADMIN_ADDRESS=thehub@youthmarketing.com
+#MAIL_DEV_ADDRESS=jeremy.litten@gmail.com,jon@linesandwaves.com
+#MAIL_SWAP_ADMIN_TO_DEV=True
+FACEBOOK_APP_ID=326108764242439
+BASE_DOMAIN=thehub.web
+COOKIE_VALIDATION_KEY=ogCtZNMnOLWhAKDp6x9dFNFc0iJo5wpI2
+APP_DOMAIN=thehub.web
+APP_URL=http://thehub.web
+STATIC_DOMAIN=thehub.web
+STATIC_URL=http://thehub.web
+EOF
+	fi
+
+fi
+
+exec "$@"
