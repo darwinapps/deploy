44f72b948f984e12a1743fc2741123a55dc0beee
Robert Bodych <robert@bodych.eu>
2020-06-16 17:10:37 +0200

    fixing local settings

--- scripts/drupal8/docker-entrypoint.sh
+++ scripts/drupal8/docker-entrypoint.sh
@@ -30,12 +30,14 @@ if [[ "$1" == apache2* ]] || [ "$1" == php-fpm ]; then
 			echo -e "\$conf['theme_debug'] = TRUE;\n" >> $settingsf
 		fi
 
-		echo -e "\$settings['hash_salt'] = \"Dq7Y_ipY3UsSdf23q5VsQuJa2OIjuOicQ_zOumlF4gQsb9Hvh1WW_a5-55IskNO0GibY26aBKQ\";\n\n" >> $settingsf
     echo -e "\n"\
 "\$local_settings = __DIR__ . '/settings.local.php';\n" \
-"if (file_exists($local_settings)) {\n" \
-"  include $local_settings;\n" \
+"if (file_exists(\$local_settings)) {\n" \
+"  include \$local_settings;\n" \
 "}\n"  >> $settingsf
+
+		echo -e "\$settings['hash_salt'] = \"Dq7Y_ipY3UsSdf23q5VsQuJa2OIjuOicQ_zOumlF4gQsb9Hvh1WW_a5-55IskNO0GibY26aBKQ\";\n\n" >> $settingsf
+
 		chown "$user:$group" $settingsf;
 	fi
 
