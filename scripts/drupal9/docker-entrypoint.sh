d6c08e10a4385e00fd94734310884efc0cd12a4b
Semashko Denis <dsemashko@darwinapps.com>
2020-08-28 12:12:12 +0300

    Add dockerfile Drupal 9

---
+++ scripts/drupal9/docker-entrypoint.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# ps auxww
+# echo $@
+# export
+
+set -euo pipefail
+
+if [[ "$1" == apache2* ]] || [ "$1" == php-fpm ]; then
+	if [ "$(id -u)" = '0' ]; then
+		case "$1" in
+			apache2*)
+				user="${APACHE_RUN_USER:-www-data}"
+				group="${APACHE_RUN_GROUP:-www-data}"
+				;;
+			*) # php-fpm
+				user='www-data'
+				group='www-data'
+				;;
+		esac
+	else
+		user="$(id -u)"
+		group="$(id -g)"
+	fi
+
+	settingsf="/var/www/html/${APP_ROOT}sites/default/settings.php";
+	if [ ! -f $settingsf ]; then
+		cp -a /var/www/html/${APP_ROOT}sites/default/default.settings.php $settingsf
+		if [ ! -z "${DEBUG:-}" ]; then
+			echo -e "\$conf['theme_debug'] = TRUE;\n" >> $settingsf
+		fi
+
+    echo -e "\n"\
+"\$local_settings = __DIR__ . '/settings.local.php';\n" \
+"if (file_exists(\$local_settings)) {\n" \
+"  include \$local_settings;\n" \
+"}\n"  >> $settingsf
+
+		echo -e "\$settings['hash_salt'] = \"Dq7Y_ipY3UsSdf23q5VsQuJa2OIjuOicQ_zOumlF4gQsb9Hvh1WW_a5-55IskNO0GibY26aBKQ\";\n\n" >> $settingsf
+
+		chown "$user:$group" $settingsf;
+	fi
+
+fi
+
+
+exec "$@"
